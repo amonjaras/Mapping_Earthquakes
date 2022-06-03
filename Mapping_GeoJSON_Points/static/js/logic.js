be1dae0f00788121b0f5ab5879aa9e11793ac660
amonjaras <100452376+amonjaras@users.noreply.github.com>
2022-06-03 16:35:53 -0400

    OnEachFeature Skill Drill 2

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,47 +1,86 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([30.1975, -97.6665], 5);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// Coordinates for each point to be used in the line.
-let line = [
-    //[33.9416, -118.4085],   //LAX
-    [37.6213, -122.3790],   //SFO
-    //[40.7899, -111.9791],   //SLC
-    //[47.4502, -122.3088],   //SEA
-    [30.1975, -97.6665],    //AUS
-    [43.6777, -79.6248],    //YYZ
-    [40.6415, -73.7783]     //JFK
-  ];
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
 
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "blue",
-    opacity: 0.5,
-    weight: 4,
-    dashArray: "10, 10",
-    dashOffset: "0"
-  }).addTo(map);
+/* // Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+  // We turn each feature into a marker on the map.
+  pointToLayer: function(feature, latlng) {
+    console.log(feature);
+    return L.marker(latlng)
+    .bindPopup("<h2>" + feature.properties.city + "</h2>");
+  }
+
+}).addTo(map); */
+
+/* //Skill Drill 1
+//Edit your logic.js to create a popup marker for San Francisco Airport 
+//on a night preview navigation map. When you click on the popup, it will 
+//display the city, state, and the name of the airport.
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+  // We turn each feature into a marker on the map.
+  pointToLayer: function(feature, latlng) {
+    console.log(feature);
+    return L.marker(latlng)
+    .bindPopup("<h2>" + feature.properties.name + "</h2><h3>" 
+    + feature.properties.city + ", " + feature.properties.country + "</h3>");
+  }
+
+}).addTo(map); */
+
+/* // Grabbing our GeoJSON data. (OnEachFeature)
+L.geoJSON(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup();
+   }
+}); */
+
+//Skill Drill 2
+//Edit your logic.js to create a popup marker for the San Francisco Airport 
+//on the outdoor map. When you click on the popup, it will display the airport 
+//code and name of the airport.
+
+// Grabbing our GeoJSON data. (OnEachFeature)
+L.geoJSON(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup("<h2>" + "Code: " + feature.properties.faa + "</h2><h3>" 
+    + feature.properties.name + "</h3>");
+   }
+});
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {  
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
-
-// Final code based on Skill Drill
-/* Edit your logic.js to create an airline route from SFO to 
-John F. Kennedy International Airport (JFK) with two stops, 
-Austin-Bergstrom International Airport (AUS) and 
-Toronto Pearson International Airport (YYZ). Make the route 
-a blue dashed line, with a weight of 4 and opacity of 0.5 on the 
-light map.
-
-Hint: You'll need to find the coordinates for some of these airports.
-
-Bonus: Add your city or another city as a stopping point.
- */
